563bf268358a8e2beb6ae5e801067e73c88a51df
dirkknibbe <dirk.knibbe@yahoo.com>
2022-08-25 14:52:51 -0500

    added back nomic labs stuff and finished hardhat config file

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,4 +1,13 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/qj0Xun4aBLtqsK0pP1KbRJe9KSyYkeAI",
+      accounts: [
+        "69200facc0fd3c6cacd5f5b97c315e269dbe12ad20b2671fa4751e5fbaeb6975",
+      ],
+    },
+  },
 };
