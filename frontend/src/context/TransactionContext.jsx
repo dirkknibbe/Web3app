8915b1618ebc63af36a5d7b69a57422c8026a4e9
dirkknibbe <dirk.knibbe@yahoo.com>
2022-09-09 17:20:31 -0500

    modified welcome component and added transaction context

---
+++ frontend/src/context/TransactionContext.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+import { contractABI, contractAddress } from "../utils/constants.js";
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+
+  console.log({ provider, signer, transactionContract });
+};
+
+export const TransactionProvider = ({ children }) => {
+  const [connectedAccount, setConnectedAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    if (!ethereum) return alert("Please install Metamask");
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    console.log(accounts);
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("Please install Metamask");
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <TransactionContext.Provider value={{}}>
+      {children}
+    </TransactionContext.Provider>
+  );
+};
