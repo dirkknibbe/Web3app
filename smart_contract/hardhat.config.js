9648679e34f8e319e35ec1cb4e00bb6286002d6a
dirkknibbe <dirk.knibbe@yahoo.com>
2022-09-12 12:19:22 -0500

    added alchemy url to env

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/iFM6D_woRtn8zAjMqFH_fK1_mo4OdauS",
+      url: `${process.env(URL)}`,
       accounts: [`${process.env(ACCOUNT)}`],
     },
   },
