df30a99f0bbd2515fcf8a5e50eec9672405716e6
dirkknibbe <dirk.knibbe@yahoo.com>
2022-09-14 16:49:41 -0500

    design and styles finished roughly

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -24,7 +24,14 @@ const Footer = () => {
       </div>
       <div className="flex justify-center items-center flex-col mt-5">
         <p className="text-white text-sm text-center">Join me!</p>
-        <p className="text-white text-sm text-center">dirkdevelops.com</p>
+        <a href="https://dirkdevelops.com">
+          <p className="text-white text-sm text-center">dirkdevelops.com</p>
+        </a>
+      </div>
+      <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5" />
+      <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+        <p className="text-white text-sm text-center">@Dirk Knibbe 2022</p>
+        <p className="text-white text-sm text-center">All Rights Reserved</p>
       </div>
     </div>
   );
